c6f05b391eb8181cb09057d79ccce7c6cadb1ce8
Russ Ferriday <topiaruss@gmail.com>
2018-06-15 12:39:01 +0100

    generate and show permutated tiles.

--- dataset/dicom_jigsaw_loader.py
+++ dataset/dicom_jigsaw_loader.py
@@ -12,9 +12,10 @@ import torch.utils.data as data
 import torchvision.transforms as transforms
 from PIL import Image
 
-TILE_INFO_FILE = 'stored_context.pickle'
+CONTEXT_FILE = 'stored_context.pickle'
 JIGSAWS = 120
 VALFRAC = 0.25
+BLOCK_SIDE = 255
 
 
 def partition_train_val_sets(data):
@@ -35,13 +36,20 @@ def normalise(img, bits):
     return img / (2 ** bits - 1)
 
 
-def define_random_tiles(path, side, count, maxtries=1000):
-    """Returns a descriptor for up to count tiles that have at least 0.75 coverage"""
+def get_defined_block(row):
+    """given a row from data, return the block"""
+    ds = pydicom.dcmread(row['path'])
+    layer, x, y, side = row['layer'], row['x'], row['y'], row['side']
+    return ds.pixel_array[layer][x:x + side, y:y + side]
+
+
+def define_random_blocks(path, side, count, maxtries=1000):
+    """Returns a descriptor for up to count blocks that have at least 0.75 coverage"""
     ds = pydicom.dcmread(path)
     layers, x, y = ds.pixel_array.shape
     max_x, max_y = x - side, y-side
     tries = 0
-    tiles = []
+    blocks = []
     while tries < maxtries:
         tries += 1
         layer, x, y = random.randrange(layers), random.randrange(max_x), random.randrange(max_y)
@@ -54,10 +62,10 @@ def define_random_tiles(path, side, count, maxtries=1000):
             continue
         min, mean, max = norm.min(), norm.mean(), norm.max()
         print('cov: %4.3f, min: %4.3f, max %4.3f, mean %4.3f, tries %d' % (mask_mean, min, max, mean, tries))
-        tiles.append(dict(path=path, x=x, y=y, coverage=mask_mean))
-        if len(tiles) >= count:
+        blocks.append(dict(path=path, layer=layer, x=x, y=y, side=side, coverage=mask_mean))
+        if len(blocks) >= count:
             break
-    return tiles
+    return blocks
 
 
 def study_date_to_iso(sd):
@@ -91,43 +99,61 @@ class DicomDataset(data.Dataset):
             # std =[0.229, 0.224, 0.225])
         ])
         # we have computed/retrieved the train and val lists, now we select which we need
-        self.mytiles = self.tiles['train'] if train else self.tiles['val']
+        self.myblocks = self.blocks['train'] if train else self.blocks['val']
 
     def __getitem__(self, index):
-        framename = self.data_path + '/' + self.names[index]
+        """
 
-        img = Image.open(framename).convert('RGB')
-        if np.random.rand() < 0.30:
-            img = img.convert('LA').convert('RGB')
+        :param index:
+        :return:
+        BORD = pixel border
+        SIDE = side of output square
+        """
+        BORD = 5
+        SIDE = 75
 
-        if img.size[0] != 255:
-            img = self.__image_transformer(img)
-
-        s = float(img.size[0]) / 3
-        a = s / 2
+        img = get_defined_block(self.myblocks[index])
+        s = img.shape[0] / 3.0
         tiles = [None] * 9
+
         for n in range(9):
-            i = n / 3
+            i = n // 3
             j = n % 3
-            c = [a * i * 2 + a, a * j * 2 + a]
-            c = np.array([c[1] - a, c[0] - a, c[1] + a + 1, c[0] + a + 1]).astype(int)
-            tile = img.crop(c.tolist())
-            tile = self.__augment_tile(tile)
-            # Normalize the patches indipendently to avoid low level features shortcut
-            m, s = tile.view(3, -1).mean(dim=1).numpy(), tile.view(3, -1).std(dim=1).numpy()
-            s[s == 0] = 1
-            norm = transforms.Normalize(mean=m.tolist(), std=s.tolist())
-            tile = norm(tile)
+            orig = [int(s * i + BORD), int(s * j + BORD)]
+            end = [int(o + SIDE) for o in orig]
+            tile = img[orig[0]:end[0], orig[1]:end[1]]
+            # print(i, j, orig, end, tile.shape)
+            # Normalize the patches independently to avoid low level features shortcut
+            tile = torch.from_numpy(tile/tile.max())
             tiles[n] = tile
 
         order = np.random.randint(len(self.permutations))
         data = [tiles[self.permutations[order][t]] for t in range(9)]
         data = torch.stack(data, 0)
+        return data, order, tiles
 
-        return data, int(order), tiles
+    def plot_item(self, index):
+        """Plots 18 squares. First 9 are the permuted tiles. Last 9 are original.
+        Prints to console the permutation index and permutation.
+        You can see how the permutation dictates the placement of the tiles from the
+        last 9 into the top 9 tiles.
+
+        :param index: index into the dataset
+
+        """
+        data, order, tiles = self[index]
+        fig = plt.figure(figsize=(6, 3))
+        print("order: %s, %s" % (order, self.permutations[order]))
+        for i in range(9):
+            fig.add_subplot(6, 3, i+1)
+            plt.imshow(data[i])
+        for i in range(9):
+            fig.add_subplot(6, 3, i+1+9)
+            plt.imshow(tiles[i])
+        plt.show()
 
     def __len__(self):
-        return len(self.mytiles)
+        return len(self.myblocks)
 
     def _dicom_info(self, path, verbose=False):
         """extract info from file at path."""
@@ -201,39 +227,43 @@ class DicomDataset(data.Dataset):
         return all_perm
 
     def _retrieve_context(self):
-        f = TILE_INFO_FILE
         try:
-            self.context = pickle.load(open(f, 'rb'))
-            print('loaded context from %s' % f)
-            # check the set of files is still the same as when tiles computed
-            assert self.data == self.context['data']
-            self.tiles = self.context['tiles']
+            self.context = pickle.load(open(CONTEXT_FILE, 'rb'))
+            print('loaded context from %s' % CONTEXT_FILE)
+            # check the set of files is still the same as when blocks computed
+            pickled = self.context['data']
+            fresh = self.data
+            diffs = [(a, fresh[i]) for i, a in enumerate(pickled) if a != fresh[i]]
+            print('DIFFS:', diffs)
+            assert not diffs
+            self.blocks = self.context['blocks']
             self.data = self.context['data']
+            self.from_context = True
         except IOError as ex:
-            print("failed loading context from %s, so we'll compute afresh" % f)
+            print("failed loading context from %s, so we'll compute afresh" % CONTEXT_FILE)
             print(ex)
-            self._compute_tiles()
-            context = dict(tiles=self.tiles, data=self.data)
-            pickle.dump(context, open(TILE_INFO_FILE, 'wb'))
+            self._compute_blocks()
+            context = dict(blocks=self.blocks, data=self.data)
+            pickle.dump(context, open(CONTEXT_FILE, 'wb'))
 
-    def _compute_tiles(self):
+    def _compute_blocks(self):
         self.train_files, self.val_files = partition_train_val_sets(self.data)
-        self.tiles = dict(train=[], val=[])
-        print('compute train tiles')
-        while len(self.tiles['train']) < JIGSAWS:
+        self.blocks = dict(train=[], val=[])
+        print('compute train blocks')
+        while len(self.blocks['train']) < JIGSAWS:
             for f in self.train_files:
                 print(f)
                 try:
-                    self.tiles['train'].extend(define_random_tiles(f, 255, 5))
+                    self.blocks['train'].extend(define_random_blocks(f, BLOCK_SIDE, 5))
                 except TypeError as ex:
                     print("failed reading file %s" % f)
                     print(ex)
-        print('compute val tiles')
-        while len(self.tiles['val']) < JIGSAWS:
+        print('compute val blocks')
+        while len(self.blocks['val']) < JIGSAWS:
             for f in self.val_files:
                 print(f)
                 try:
-                    self.tiles['val'].extend(define_random_tiles(f, 255, 5))
+                    self.blocks['val'].extend(define_random_blocks(f, BLOCK_SIDE, 5))
                 except TypeError as ex:
                     print("failed reading file %s" % f)
                     print(ex)
--- test_dicom_ops.py
+++ test_dicom_ops.py
@@ -1,9 +1,11 @@
 import pydicom
 import tablib
+import os
 
 from .dataset.dicom_jigsaw_loader import (partition_train_val_sets,
-                                          define_random_tiles,
-                                          DicomDataset)
+                                          define_random_blocks,
+                                          DicomDataset,
+                                          CONTEXT_FILE)
 
 
 def test_partition_test_val_sets():
@@ -17,14 +19,52 @@ def test_partition_test_val_sets():
     assert len(t) + len(v) == len(data)
 
 
-def test_select_random_tiles():
+def test_select_random_blocks():
     ff = '/home/pc1/data/PEONY-2018-06/patients/HCA217594/2018-05-02/PAT01/BTO/BTO03'
-    tiles = define_random_tiles(ff, 255, 10)
-    assert len(tiles) == 10
+    blocks = define_random_blocks(ff, 255, 10)
+    assert len(blocks) == 10
 
-def test_dicom_instance():
+
+def test_dicom_instance_clean():
     dp = """/home/pc1/data/PEONY-2018-06/patients/HCA110723/
             /home/pc1/data/PEONY-2018-06/patients/HCA18595/
             /home/pc1/data/PEONY-2018-06/patients/HCA265541/
             /home/pc1/data/PEONY-2018-06/patients/HCA217594/"""
+    if os.path.isfile(CONTEXT_FILE):
+        os.remove(CONTEXT_FILE)
     dds = DicomDataset(dp)
+
+
+def test_dicom_instance_retrieve():
+    # lazily assumes that context will have been dumped in previous test
+    dp = """/home/pc1/data/PEONY-2018-06/patients/HCA110723/
+            /home/pc1/data/PEONY-2018-06/patients/HCA18595/
+            /home/pc1/data/PEONY-2018-06/patients/HCA265541/
+            /home/pc1/data/PEONY-2018-06/patients/HCA217594/"""
+    assert os.path.isfile(CONTEXT_FILE)
+    dds = DicomDataset(dp)
+    assert hasattr(dds, 'from_context')
+
+
+def test_get_items():
+    dp = """/home/pc1/data/PEONY-2018-06/patients/HCA110723/
+            /home/pc1/data/PEONY-2018-06/patients/HCA18595/
+            /home/pc1/data/PEONY-2018-06/patients/HCA265541/
+            /home/pc1/data/PEONY-2018-06/patients/HCA217594/"""
+    assert os.path.isfile(CONTEXT_FILE)
+    dds = DicomDataset(dp)
+    assert hasattr(dds, 'from_context')
+
+    jj = dds[0]
+    pass
+
+
+def test_get_items():
+    dp = """/home/pc1/data/PEONY-2018-06/patients/HCA110723/
+            /home/pc1/data/PEONY-2018-06/patients/HCA18595/
+            /home/pc1/data/PEONY-2018-06/patients/HCA265541/
+            /home/pc1/data/PEONY-2018-06/patients/HCA217594/"""
+    assert os.path.isfile(CONTEXT_FILE)
+    dds = DicomDataset(dp)
+    assert hasattr(dds, 'from_context')
+    dds.plot_item(0)
